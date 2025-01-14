a2b71a8dff15c2d507f31e9c8e94da65b9ca085f
326890 <enderdissa@niuitmo.ru>
2025-01-14 12:01:58 +0300

    added initial setup settings

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import getpass
+
+required_dirs = [
+    'xlsx'
+]
+
+def create():
+    for directory in required_dirs:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            print(f"Создана папка: {directory}")
+
+    if not os.path.exists('token.txt'):
+        token=getpass.getpass("введи токен приложения VK:\n")
+        with open ('token.txt', 'w') as f:
+            f.write(token)
+        print(f"токен успешно записан")
+
+
+if __name__ == "__main__":
+    create()

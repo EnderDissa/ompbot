651a5a0b42ea4f95dfe06a011934c357f4c78b23
Denis Berman <secondissa@yandex.ru>
2025-08-31 17:27:16 +0300

    updated very old path generaton logic

--- utils/net_helper.py
+++ utils/net_helper.py
@@ -9,9 +9,9 @@ def attachment_extract(url, name, ext):
     response = requests.get(url)
 
     if not os.path.exists(ext + '/' + name):
-        dir = ext +"/" + name
+        dir = 'data/' + ext +"/" + name
         os.mkdir(dir)
-    path = ext + "/" + name + "/" + ("_".join(str(date.now())[:-7].replace(":", "-").split())) + "." + ext
+    path = 'data/' + ext + "/" + name + "/" + ("_".join(str(date.now())[:-7].replace(":", "-").split())) + "." + ext
     with open(path, "wb") as f:
         f.write(response.content)
         return path
